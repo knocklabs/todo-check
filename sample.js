009e3972ea3360ace1e4c3909a93f12a704316d0
Mike Carbone <mfcbone@gmail.com>
2025-03-27 14:17:01 -0400

    fix: editing comments (#3)
    * feat: adds console.log
    
    * testing editing comments
    
    * Fixes comment editing

--- sample.js
+++ sample.js
@@ -4,14 +4,15 @@ function exampleFunction() {
   console.log('Hello world');
   
   // TODO: This needs to be fixed in KNO-1234
-  const tempValue = 'temporary implementation';
-  console.log('Added a log statement for KNO-5678 test');
+  const tempValue = 'temporary implementation'; g
   
+  // This is another non-t-o-d-o comment mentioning KNO-5678
   return tempValue;
 }
 
 // TODO: Refactor this entire file for kno-5678
 function anotherFunction() {
+  console.log('Added a log statement for a new KNO-5678 test');
   return 'placeholder';
 }
 
---
+++ second-sample.js
@@ -0,0 +1,7 @@
+function exampleFunction() {
+  console.log('Hello world');
+
+  // TODO: This needs to be fixed in kno-5678
+  const tempValue = 'temporary implementation';
+  return tempValue;
+}
